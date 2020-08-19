a4e16228343976f237499be3323b38e843b22bc5
SaadMMansouri <mansouri.m.saad@gmail.com>
2020-08-19 13:49:35 +0100

    Initial Commit

---
+++ SwiftUIProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIProject
+//
+//  Created by Saad Mansouri on 8/19/20.
+//  Copyright © 2020 Saad Mansouri. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
