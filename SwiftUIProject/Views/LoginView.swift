cc3c25e34bc165e8a0449c503b5de326d8cf5018
SaadMMansouri <mansouri.m.saad@gmail.com>
2020-08-20 19:22:04 +0100

    Add LoginView's UI and contained/text reusable buttons

---
+++ SwiftUIProject/Views/LoginView.swift
@@ -0,0 +1,83 @@
+//
+//  LoginView.swift
+//  SwiftUIProject
+//
+//  Created by Saad Mansouri on 8/20/20.
+//  Copyright © 2020 Saad Mansouri. All rights reserved.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    
+    @State private var signInIsVisible = false
+
+    var body: some View {
+        VStack{
+            Spacer()
+            VStack {
+                Image("InstagramLogo")
+                    .resizable()
+                    .scaledToFit()
+                    .clipped()
+                    .frame(height : 200)
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .edgesIgnoringSafeArea(.top)
+                
+                TextField("Phone, email or username", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                    .padding()
+                    .background(Color.clear)
+                    .cornerRadius(4)
+                TextField("Password", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                    .padding()
+                    .background(Color.clear)
+                    .cornerRadius(4)
+                
+                ContainedButtonView(
+                    text : "Log In",
+                    color : Color.blue,
+                    textColor: Color.white,
+                    action: {}
+                )
+                TextButtonView(
+                    sentence: "forgot your login details?",
+                    text : "Get help signing in.",
+                    action: {}
+                ).frame(height : 10).padding()
+                
+                ZStack{
+                    Divider().frame(minWidth: 0, maxWidth: .infinity)
+                    Text("OR").font(.system(size: 15))
+                        .padding(10)
+                        .background(Color.white)
+                }
+                
+                ContainedButtonView(
+                    text : "Log in with facebook",
+                    color : Color.blue,
+                    textColor: Color.white,
+                    action: {}
+                )
+
+            }.padding()
+            Spacer()
+            Divider().frame(minWidth: 0, maxWidth: .infinity)
+            TextButtonView(
+                sentence: "Dont have an account?",
+                text : "Sign up.",
+                action: {
+                    self.signInIsVisible.toggle()
+                }
+            ).padding(.leading).padding(.trailing)
+            .sheet(isPresented: $signInIsVisible, content: {Text("Sign up now")})
+
+        }
+        
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
---
+++ SwiftUIProject/Views/ReusableViews/ContainedButtonView.swift
@@ -0,0 +1,33 @@
+//
+//  ContainedButtonView.swift
+//  SwiftUIProject
+//
+//  Created by Saad Mansouri on 8/20/20.
+//  Copyright © 2020 Saad Mansouri. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContainedButtonView: View {
+    
+    var text : String = "Button"
+    var color : Color = .blue
+    var textColor : Color = .white
+    var action : () -> Void
+    
+    var body: some View {
+        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+            Text(text)
+            .fontWeight(.bold)
+            .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
+            .foregroundColor(textColor)
+            .padding()
+        }.background(color).cornerRadius(4)
+    }
+}
+
+struct ContainedButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContainedButtonView( action: {})
+    }
+}
